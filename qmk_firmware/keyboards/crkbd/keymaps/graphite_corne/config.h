9c9d59f206fa52da9303bb604a98c8bdda8c5a7a
Matthew Hillman <matthewahillman@icloud.com>
2026-04-27 16:25:03 +0100

    Revert Corne OLED customisations to original behaviour

--- qmk_firmware/keyboards/crkbd/keymaps/graphite_corne/config.h
+++ qmk_firmware/keyboards/crkbd/keymaps/graphite_corne/config.h
@@ -3,7 +3,3 @@
 // VIA default dynamic layers are lower on some targets.
 // This keymap uses 5 layers (Base/Nav/Sym/Num/Fn).
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-
-// Force plain, readable OLED text rendering.
-#define OLED_DISPLAY_128X32
-#define OLED_FONT_H "drivers/oled/glcdfont.c"
--- qmk_firmware/keyboards/crkbd/keymaps/graphite_corne/rules.mk
+++ qmk_firmware/keyboards/crkbd/keymaps/graphite_corne/rules.mk
@@ -1,5 +1,4 @@
 VIA_ENABLE = yes
-OLED_ENABLE = yes
 
 # Optional QMK-only enhancements (off by default).
 # COMBO_ENABLE = yes
